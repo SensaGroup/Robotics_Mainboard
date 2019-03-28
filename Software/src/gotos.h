cc162e6fc7a2cec19774a79620a68e6781825317
Silard Gal <silardgal@Kepplers-MacBook-Pro.local>
2019-03-28 16:17:46 +0100

    gotos fancy comments

--- Software/src/gotos.h
+++ Software/src/gotos.h
@@ -3,6 +3,14 @@
 
 #include "system.h"
 
+
+/*        ____    ____  _______  __       __        ______   ____    __    ____ 
+      \   \  /   / |   ____||  |     |  |      /  __  \  \   \  /  \  /   / 
+       \   \/   /  |  |__   |  |     |  |     |  |  |  |  \   \/    \/   /  
+        \_    _/   |   __|  |  |     |  |     |  |  |  |   \            /   
+          |  |     |  |____ |  `----.|  `----.|  `--'  |    \    /\    /    
+          |__|     |_______||_______||_______| \______/      \__/  \__/               */
+          
 const struct odometry_position yellow_table_one_starter = {0,0,0};
 const struct goto_fields yellow_table_one[]             = {
     {{0,0}, 50, FORWARD, NULL,NULL}
@@ -13,11 +21,12 @@ const struct goto_fields yellow_table_two[]             = {
     {{0,0}, 50, FORWARD, NULL,NULL}
 };
 
-
-
-
-
-
+/*  .______    __    __  .______      .______    __       _______ 
+    |   _  \  |  |  |  | |   _  \     |   _  \  |  |     |   ____|
+    |  |_)  | |  |  |  | |  |_)  |    |  |_)  | |  |     |  |__   
+    |   ___/  |  |  |  | |      /     |   ___/  |  |     |   __|  
+    |  |      |  `--'  | |  |\  \----.|  |      |  `----.|  |____ 
+    | _|       \______/  | _| `._____|| _|      |_______||_______|  */
 
 const struct odometry_position purple_table_one_starter = {0,0,0};
 const struct goto_fields purple_table_one[]             = {
